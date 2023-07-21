e6b5e709c413776510f689b06d5398c2270ac246
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-21 00:50:56 -0700

    added local storage features and debug

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create variables for all of the time block elements using their id
 var nineEl = $("#hour-9")
 var tenEl = $("#hour-10")
 var elevenEl = $("#hour-11")
@@ -8,35 +9,67 @@ var fifteenEl = $("#hour-15")
 var sixteenEl = $("#hour-16")
 var seventeenEl = $("#hour-17")
 
+
+//  Pull previously saved values for each time block from local storage.
+if (localStorage.getItem("nineEntry") !== null) {
+  var nine = localStorage.getItem("nineEntry");
+}
+
+if (localStorage.getItem("tenEntry") !== null) {
+  var ten = localStorage.getItem("tenEntry");
+}
+
+if (localStorage.getItem("elevenEntry") !== null) {
+  var eleven = localStorage.getItem("elevenEntry");
+}
+
+if (localStorage.getItem("twelveEntry") !== null) {
+  var twelve = localStorage.getItem("twelveEntry");
+}
+
+if (localStorage.getItem("thirteenEntry") !== null) {
+  var thirteen = localStorage.getItem("thirteenEntry");
+}
+
+if (localStorage.getItem("fourteenEntry") !== null) {
+  var fourteen = localStorage.getItem("fourteenEntry");
+}
+
+if (localStorage.getItem("fifteenEntry") !== null) {
+  var fifteen = localStorage.getItem("fifteenEntry");
+}
+
+if (localStorage.getItem("sixteenEntry") !== null) {
+  var sixteen = localStorage.getItem("sixteenEntry");
+}
+
+if (localStorage.getItem("seventeenEntry") !== null) {
+  var seventeen = localStorage.getItem("seventeenEntry");
+}
+
+// Populate schedule time blocks with previously saved entries from local storage, if present
+$("#nine-text").val(nine);
+$("#ten-text").val(ten);
+$("#eleven-text").val(eleven);
+$("#twelve-text").val(twelve);
+$("#thirteen-text").val(thirteen);
+$("#fourteen-text").val(fourteen);
+$("#fifteen-text").val(fifteen);
+$("#sixteen-text").val(sixteen);
+$("#seventeen-text").val(seventeen);
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
-  // TODO: Add code to display the current date in the header of the page.
+
   // Using dayjs to get the date and display it in the element id currentDay/
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"));
 
-
-  // var thisHour = dayjs().hour();
+  // Create variables for each hour block to use in the conditional statements below 
   var thisHour = 12;
   var nineAM = 9;
   var tenAM = 10;
@@ -48,7 +81,7 @@ $(function () {
   var sixteenPM = 16;
   var seventeenPM = 17;
 
-  //Conditional statements for the time blocks 
+  //Conditional statements for time block color coding 
   if (nineAM < thisHour) {
     nineEl.attr('class', 'row time-block past');
   } else if (nineAM === thisHour) {
@@ -122,6 +155,62 @@ $(function () {
     seventeenEl.attr('class', 'row time-block future mb-4');
   };
 
+// Save button listeners. Upon clicking the save button, the value in the time block is displayed in the console and saved to local storage.
+  $("#nine-button").click(function () {
+    var nineEntry = $("#nine-text").val();
+    console.log(nineEntry);
+    localStorage.setItem("nineEntry", nineEntry);
+  })
+
+  $("#ten-button").click(function () {
+    var tenEntry = $("#ten-text").val();
+    console.log(tenEntry);
+    localStorage.setItem("tenEntry", tenEntry)
+  })
+
+  $("#eleven-button").click(function () {
+    var elevenEntry = $("#eleven-text").val();
+    console.log(elevenEntry);
+    localStorage.setItem("elevenEntry", elevenEntry)
+  })
+
+  $("#twelve-button").click(function () {
+    var twelveEntry = $("#twelve-text").val();
+    console.log(twelveEntry);
+    localStorage.setItem("twelveEntry", twelveEntry)
+  })
+
+  $("#thirteen-button").click(function () {
+    var thirteenEntry = $("#thirteen-text").val();
+    console.log(thirteenEntry);
+    localStorage.setItem("thirteenEntry", thirteenEntry)
+  })
+
+  $("#fourteen-button").click(function () {
+    var fourteenEntry = $("#fourteen-text").val();
+    console.log(fourteenEntry);
+    localStorage.setItem("fourteenEntry", fourteenEntry)
+  })
+
+  $("#fifteen-button").click(function () {
+    var fifteenEntry = $("#fifteen-text").val();
+    console.log(fifteenEntry);
+    localStorage.setItem("fifteenEntry", fifteenEntry)
+  })
+
+  $("#sixteen-button").click(function () {
+    var sixteenEntry = $("#sixteen-text").val();
+    console.log(sixteenEntry);
+    localStorage.setItem("sixteenEntry", sixteenEntry)
+  })
+
+  $("#seventeen-button").click(function () {
+    var seventeenEntry = $("#seventeen-text").val();
+    console.log(seventeenEntry);
+    localStorage.setItem("seventeenEntry", seventeenEntry)
+  })
+
+
 });
 
-// use data- in the html for the time elements to grab data from the element for the time storage. maybe
+
