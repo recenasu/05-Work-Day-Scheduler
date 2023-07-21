a2fc28dcd470f621abdf18e986e04330366a1224
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-20 17:03:25 -0700

    working on day elements

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
+var nineEl = $("#hour-9")
+var tenEl = $("#hour-10")
+var elevenEl = $("#hour-11")
+var twelveEl = $("#hour-12")
+var thirteenEl = $("#hour-13")
+var fourteenEl = $("#hour-14")
+var fifteenEl = $("#hour-15")
+var sixteenEl = $("#hour-16")
+var seventeenEl = $("#hour-17")
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -21,10 +29,53 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  
+
   // TODO: Add code to display the current date in the header of the page.
   // Using dayjs to get the date and display it in the element id currentDay/
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"));
+
+
+  // var thisHour = dayjs().hour();
+  var thisHour = 3
+  console.log(thisHour);
+  var nineAM = 9;
+  var tenAM = 10;
+  var elevenAM = 11;
+  var twelveAM = 12;
+  console.log(nineAM);
+  console.log(twelveAM);
+  
+  if (thisHour > nineAM) {
+    nineEl.attr('class', 'row time-block past');
+  } else if (thisHour = nineAM) {
+    nineEl.attr('class', 'row time-block present');
+  } else if (thisHour < nineAM){
+    nineEl.attr('class', 'row time-block future');
+  };
+
+  if (thisHour > tenAM) {
+    tenEl.attr('class', 'row time-block past');
+  } else if (thisHour = tenAM) {
+    tenEl.attr('class', 'row time-block present');
+  } else if (thisHour < tenAM){
+    tenEl.attr('class', 'row time-block future');
+  };
+
+  if (thisHour > elevenAM) {
+    elevenEl.attr('class', 'row time-block past');
+  } else if (thisHour = elevenAM) {
+    elevenEl.attr('class', 'row time-block present');
+  } else if (thisHour < elevenAM){
+    elevenEl.attr('class', 'row time-block future');
+  };
+
+  if (thisHour > twelveAM) {
+    twelveEl.attr('class', 'row time-block past');
+  } else if (thisHour = twelveAM) {
+    twelveEl.attr('class', 'row time-block present');
+  } else if (thisHour < twelveAM){
+    twelveEl.attr('class', 'row time-block future');
+  };
 
 });
